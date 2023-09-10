b6ef58aea30f5ca9f002429807a37f8c1727ecec
Omer Haimovich <omer16493@gmail.com>
2023-09-10 16:36:19 +0300

    Change driver color when picked up a package

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
 
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -17,12 +26,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
         if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("delivered package");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
