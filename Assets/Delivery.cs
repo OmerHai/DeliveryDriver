b9d75d14459cf371d3202ed9554fef8b7b926f70
Omer Haimovich <omer16493@gmail.com>
2023-09-10 16:07:32 +0300

    Fix package delivery to customer before collected the package itself

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -11,9 +12,14 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Package")
+        {
             Debug.Log("package picked up");
-        if (other.tag == "Customer")
+            hasPackage = true;
+        }
+        if (other.tag == "Customer" && hasPackage)
+        {
             Debug.Log("delivered package");
-
+            hasPackage = false;
+        }
     }
 }
