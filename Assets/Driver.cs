05596c67c44fa0b4cd2064e192e4f9c7a3be1336
Omer Haimovich <omer16493@gmail.com>
2023-09-10 16:49:10 +0300

    Add boosts and bumps system

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,11 +4,8 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300f;
     [SerializeField] float moveSpeed = 20f;
-
-    void Start()
-    {
-
-    }
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
     void Update()
     {
@@ -17,4 +14,16 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SpeedUp")
+            moveSpeed = boostSpeed;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
 }
