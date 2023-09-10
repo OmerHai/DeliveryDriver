55d3932abc0e20576e6bd604af7111e56793b9e7
Omer Haimovich <omer16493@gmail.com>
2023-09-10 16:20:13 +0300

    Create destroy package after picked up

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -11,10 +13,11 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("package picked up");
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
         if (other.tag == "Customer" && hasPackage)
         {
